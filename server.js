94100a662fe6e55a0a24c95e5292cca44d223d8e
Elias Bernhaut <ebernhaut@itbernhaut.ch>
2022-12-04 19:36:11 +0100

    Update socket.io

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-var server = require('http').Server(app);
-const io = require('socket.io')(server);
-const ServerInfoCache = require('./cache');
+var server = require("http").Server(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: process.env.CLIENT_URL,
+        credentials: true
+    }
+});
+const ServerInfoCache = require("./cache");
 
 const cache = new ServerInfoCache(5000);
 
@@ -11,46 +16,46 @@ let numClients = 0;
 
 let addresses = [];
 
-cache.on('updated', serverInfo => {
-    io.sockets.emit('updated', serverInfo);
+cache.on("updated", (serverInfo) => {
+  io.sockets.emit("updated", serverInfo);
 });
 
-io.on('connection', socket => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    socket.emit('updated', cache.getServerInfo());
+  socket.emit("updated", cache.getServerInfo());
 
-    const address = socket.request.connection.remoteAddress;
+  const address = socket.request.connection.remoteAddress;
 
-    numClients++;
+  numClients++;
 
-    addresses.push(address);
+  addresses.push(address);
 
-    cache.startUpdating();
+  cache.startUpdating();
 
-    socket.on('disconnect', () => {
-        console.log('a user disconnected');
-        numClients--;
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+    numClients--;
 
-        const index = addresses.indexOf(address);
-        addresses.splice(index, 1);
+    const index = addresses.indexOf(address);
+    addresses.splice(index, 1);
 
-        if (!numClients) cache.stopUpdating();
-    });
+    if (!numClients) cache.stopUpdating();
+  });
 });
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-    res.json(cache.getServerInfo());
+app.get("/", (req, res) => {
+  res.json(cache.getServerInfo());
 });
 
-app.get('/users', (req, res) => {
-    res.json({
-        numClients,
-        addresses
-    });
+app.get("/users", (req, res) => {
+  res.json({
+    numClients,
+    addresses,
+  });
 });
 
-server.listen(process.env.PORT || 8080, function() {
-    console.log(`Example app listening on port ${process.env.PORT || 8080}!`);
+server.listen(process.env.PORT || 8080, function () {
+  console.log(`Example app listening on port ${process.env.PORT || 8080}!`);
 });
